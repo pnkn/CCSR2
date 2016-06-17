df3e7867a2fde20c1cdce30b9b98713f13d66686
pnkn <mwhammond@hotmail.com>
2016-06-16 21:26:13 -0400

    Deleting duplicate code :/
    idk why this happens at all...

--- randomFunctions.py
+++ randomFunctions.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from main import *
 import pygame
 
@@ -10,16 +9,4 @@ def message_to_screen(text, color):
     screen_text = font.render(text, True, color)
     game_screen.blit(screen_text, [150, 150])
     pygame.display.flip()
-=======
-from main import *
-import pygame
-
-# creating font object for message_to_screen function
-font = pygame.font.SysFont(None, 25)
-
-
-def message_to_screen(text, color):
-    screen_text = font.render(text, True, color)
-    game_screen.blit(screen_text, [150, 150])
-    pygame.display.flip()
->>>>>>> origin/master
+	
